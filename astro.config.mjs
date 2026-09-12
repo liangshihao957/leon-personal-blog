bebbcefc1429e279c126ce7b72f402ab575fb6df
liangshihao <957994420@qq.com>
2026-09-12 16:39:31 +0800

    chore: 站点域名改为 liangshihao.store

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://leon-personal-blog.pages.dev',
+  site: 'https://liangshihao.store',
   trailingSlash: 'always',
   integrations: [sitemap()],
   i18n: {
